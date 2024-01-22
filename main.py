453a48e3b974e065fe37dc0a7d9b732393c6106d
ritesh-blog27 <riteshminecraft27@gmail.com>
2024-01-22 14:58:07 +0530

    create api for heart prediction

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, url_for
+import json
 import tensorflow as tf
 app = Flask(__name__)
 import os 
 
-PORT = os.environ['PORT'] or 3000;
+PORT = 3000
+# PORT = os.environ['PORT'] or 3000
 model = tf.keras.models.load_model('./Heart_Prediction_model_1.h5')
 @app.route('/')
 def home():
@@ -14,6 +16,17 @@ def home():
     ))
     return f"{pred[0]}"
 
+@app.route('/heart', methods=['POST'])
+def heart():
+    data = request.get_json()
+    inputs = request.json['inputs']
+    inputs = [float(i) for i in inputs]
+    hp_model = tf.keras.models.load_model('./Heart_Prediction_model_1.h5')
+    # pred = hp_model.predict([float(data.inputs)])
+    # print(json.loads(data))
+    pred = hp_model.predict([inputs])
+    value = pred[0][0]
+    return {'value': float(value)}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=PORT)
+    app.run(debug=True, host='0.0.0.0', port=PORT)
